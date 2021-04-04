d1c373175f87a512c5d26abe5fd9e1af35f7c7c6
Luis Chiang <lhc4@njit.edu.com>
2021-04-04 19:51:24 -0400

    base staff layout

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Dictionary<Key, KeyCode> keyToCode = new Dictionary<Key, KeyCode>{
+        {Key.C, KeyCode.S},
+        {Key.D, KeyCode.D},
+        {Key.E, KeyCode.F},
+        {Key.F, KeyCode.G},
+        {Key.G, KeyCode.H},
+        {Key.A, KeyCode.J},
+        {Key.B, KeyCode.J},
+    };
+
+    public bool IsKeyPressed(Key key)
+    {
+        return Input.GetKey( keyToCode[key] );
+    }
+    
+    public enum Key : int
+    {
+        A = 0,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        COUNT
+    }
+}
