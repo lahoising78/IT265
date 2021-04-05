8b2921d425b76b4959300371a63574f1199a7140
Luis Chiang <lhc4@njit.edu.com>
2021-04-04 20:09:51 -0400

    raw song classs

---
+++ Assets/Staff/Scripts/RawSong.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class RawSong
+{
+    private Note[] notes;
+
+    public RawSong()
+    {
+        notes = new Note[]{
+            new Note(Player.Key.C, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.D, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.E, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.F, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.G, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.A, RhythmNote.WHOLE, 4),
+            new Note(Player.Key.B, RhythmNote.WHOLE, 4),
+        };
+    }
+}
+
+public enum RhythmNote
+{
+    WHOLE,
+    HALF,
+    EIGTH,
+    SIXTEENTH
+}
+
+public struct Note
+{
+    public Player.Key key;
+    RhythmNote rhythm;
+    public int octave;
+
+    public Note(Player.Key key, RhythmNote rhythm, int octave)
+    {
+        this.key = key;
+        this.rhythm = rhythm;
+        this.octave = octave;
+    }
+}
