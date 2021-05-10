122f25e0d3a9811de414950831256d924484257a
Luis Chiang <lhc4@njit.edu.com>
2021-05-10 00:00:18 -0400

    place notes

--- Assets/Scripts/Notes/NotesLevel.cs
+++ Assets/Scripts/Notes/NotesLevel.cs
@@ -9,6 +9,7 @@ public class NotesLevel : MonoBehaviour
     [SerializeField] private GameObject wholeNotePrefab = null;
     public TMP_Text textElement = null;
     public GameObject allNotes = null;
+    public GameObject singleNote = null;
 
     private IEnumerator coroutine = null;
     private NoteSceneStates currentState = (NoteSceneStates)0;
@@ -25,31 +26,44 @@ public class NotesLevel : MonoBehaviour
             case NoteSceneStates.Welcome:
                 TransitionText("Welcome! In this section you will learn how to recognize the notes in a staff.\nPress C to continue");
                 allNotes.SetActive(false);
+                singleNote.SetActive(false);
                 break;
             
             case NoteSceneStates.ShowAllNotes:
                 TransitionText("Here are the notes that we will talk about. There are more, but after this tutorial you will be able to read all notes.\nPress C to continue");
                 allNotes.SetActive(true);
+                singleNote.SetActive(false);
                 break;
             
             case NoteSceneStates.ExplainCleff:
                 TransitionText("Notice how the notes are ordered. When you go up the staff, the note also increases in alphabetical order. For example, notice how C is at the bottom, and right above it is D, just like in the alphabet.\nPress C to continue");
+                singleNote.SetActive(false);
                 allNotes.SetActive(true);
                 break;
             
             case NoteSceneStates.ShowOutsideOfStaffExplanation:
                 TransitionText("The same holds true for the rest of the Staff, even outside the lines and spaces. Notice how D is below the first line of the Staff from bottom to top.\nPress C to continue");
+                singleNote.SetActive(false);
                 allNotes.SetActive(true);
                 break;
             
             case NoteSceneStates.ShowExtraLinesExplanation:
                 TransitionText("When the note goes beyond the Staff, and it is supposed to be in a line, extra lines are added to the Staff. See how C is placed on a line, but that line is not connected to the Staff like all the others\nPress C to continue");
+                singleNote.SetActive(false);
                 allNotes.SetActive(true);
                 break;
             
             case NoteSceneStates.ExplainActivity:
                 TransitionText("Now that you know how to identify the notes, let's try to play them. In the next activity, try to play the corresponding note with the virtual piano.\nPress C to continue");
                 allNotes.SetActive(false);
+                singleNote.SetActive(false);
+                break;
+            
+            case NoteSceneStates.PlayC3:
+                TransitionText("");
+                allNotes.SetActive(false);
+                PlaceNote(Player.Key.C);
+                singleNote.SetActive(true);
                 break;
 
             default:
@@ -62,6 +76,7 @@ public class NotesLevel : MonoBehaviour
     private void TransitionText(string text, float moveTime = 1.0f)
     {
         Color original = textElement.color;
+        original.a = 1.0f;
         Color transparent = original;
         transparent.a = 0.0f;
 
@@ -112,5 +127,14 @@ public class NotesLevel : MonoBehaviour
     {
         if(currentState >= NoteSceneStates.Congrats) return;
         SetState(currentState+1);
+    }
+
+    private void PlaceNote(Player.Key key)
+    {
+        Transform bar = transform.GetChild(0);
+        Vector3 notePosition = singleNote.transform.position;
+        notePosition.y = bar.GetChild((int)key-1).position.y;
+        singleNote.transform.position = notePosition;
+        singleNote.transform.GetChild(0).gameObject.SetActive(key == Player.Key.C);
     }
 }
