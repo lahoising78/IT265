85e7fe991ab52bfd217d362f433b710e5a3a18c7
Luis Chiang <lhc4@njit.edu.com>
2021-05-09 22:46:51 -0400

    notes most of the scene

---
+++ Assets/Scripts/Notes/NoteSceneStates.cs
@@ -0,0 +1,22 @@
+﻿public enum NoteSceneStates
+{
+    ShowAllNotes = 0,
+    ExplainCleff,
+    ExplainStaff,
+    ShowOutsideOfStaffExplanation,
+    ShowExtraLinesExplanation,
+    PlayC3,
+    PlayD3,
+    PlayE3,
+    PlayF3,
+    PlayG3,
+    PlayA3,
+    PlayB3,
+    GoBlind,
+    BlindC3,
+    BlindG3,
+    BlindE3,
+    BlindA3,
+    BlindB3,
+    Congrats
+}
---
+++ Assets/Scripts/Notes/NotesLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class NotesLevel : MonoBehaviour
+{
+    [SerializeField]
+    private NoteSceneStates startState = NoteSceneStates.ShowAllNotes;
+    [SerializeField] private GameObject wholeNotePrefab = null;
+
+    void Start()
+    {
+        SetState(startState);
+    }
+
+    public void SetState(NoteSceneStates state)
+    {
+        switch(state)
+        {
+            default:
+                break;
+        }
+    }
+}
